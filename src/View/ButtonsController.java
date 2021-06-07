284cb2856f24a1851cb7c4e0d670a6e3c807263b
amiryehuda <amiryehu8@gmail.com>
2021-06-07 21:14:43 +0300

    TEST

--- src/View/ButtonsController.java
+++ src/View/ButtonsController.java
@@ -1,4 +1,39 @@
 package View;
 
-public class ButtonsController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class ButtonsController implements Initializable {
+
+    @FXML
+    private Pane pane;
+
+    @FXML
+    public void openCSV(ActionEvent event){
+
+        final DirectoryChooser csv_chooser= new DirectoryChooser();
+
+        Stage stage = (Stage) pane.getScene().getWindow();
+        File file = csv_chooser.showDialog(stage);
+
+        if(file != null){
+            System.out.println("path : " + file.getAbsolutePath());
+        }
+
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        //TODO
+    }
 }
--- src/View/MainFXMLController.java
+++ src/View/MainFXMLController.java
@@ -1,4 +1,19 @@
 package View;
 
-public class MainFXMLController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainFXMLController  {
+
+
+
+
+
 }
